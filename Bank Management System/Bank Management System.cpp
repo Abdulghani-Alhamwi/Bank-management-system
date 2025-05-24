d74923addc41ef752f5c9d9d9bbf1c15294e9ee0
AbdulghaniAlhamwi <AbdulghaniAlhamwi3@gmail.com>
2025-05-24 16:32:27 +0300

    Bank Management System

---
+++ Bank Management System/Bank Management System.cpp
@@ -0,0 +1,18 @@
+// Bank Management System :
+#include <iostream>
+#include "clsLoginScreen.h"
+
+int main()
+
+{
+
+    while (true)
+    {
+        if (!clsLoginScreen::ShowLoginScreen())
+        {
+            break;
+        }
+    }
+
+    return 0;
+}
